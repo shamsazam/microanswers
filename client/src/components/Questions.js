8545dc12168f3c33535f595990683775c55c279f
dazzm <mazam@pipingrock.com>
2018-11-28 17:43:33 +0530

    separated gql query

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
+const query = gql`
+    {
+        hello
+    }`;
+
 const Questions = () => (
-    <Query
-        query={gql`
-        {
-            hello
-        }
-    `}
-    >
+    <Query query={query}>
         {({ loading, error, data }) => {
             if(loading) return <p>Loading....</p>;
             if(error) return <p>Error...</p>;
