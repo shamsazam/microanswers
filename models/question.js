ff61215514bb27f3ae6446d9c4d381377d0dbce7
dazzm <mazam@pipingrock.com>
2018-12-04 18:27:06 +0530

    added answers prop to questionSchema

--- models/question.js
+++ models/question.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const questionSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+    author: { type: ObjectId, ref: 'User', required: true },
+    answers: [{ type: ObjectId, ref: 'Answer', }]
 });
 
 module.exports = mongoose.model('Question', questionSchema);
